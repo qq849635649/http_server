4815d32f52e2a51692217f4c1318751b9f361046
zhaoyf <zhaoyf@geewise.com>
2018-04-29 20:00:33 +0800

    加载worker和master的配置

--- src/config.cpp
+++ src/config.cpp
@@ -1,7 +1,59 @@
-#include "config.h"
+#include <stdexcept>
+using namespace std;
 
-#include <libconfig/libconfig.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+#include "config.h"
 
 MConfig::MConfig()
 {
+}
+
+void MConfig::loadConfFile(const char *file)
+{
+    config_t setting = {};
+    config_init(&setting);
+
+    //const char* tmp = NULL;
+
+    if(CONFIG_TRUE != config_read_file(&setting, file))
+        throw invalid_argument("Read configure error.");
+
+    // 获取worker监听端口
+    if(getSockAddr(&setting, "Base.worker", &(base.worker_addr)))
+        throw invalid_argument("Find Base.worker error.");
+
+    // 获取master监听端口
+    if(getSockAddr(&setting, "Base.master", &(base.worker_addr)))
+        throw invalid_argument("Find Base.master error.");
+
+    config_destroy(&setting);
+}
+
+int MConfig::getSockAddr(config_t *setting, const char *conf, struct sockaddr_in *addr)
+{
+    const char* listen = NULL;
+    char ip[30] = {};
+    int port = 0;
+    if(CONFIG_TRUE != config_lookup_string(setting, conf, &listen))
+        return -1;
+
+    char * delim = strchr(const_cast<char *>(listen), ':');
+    if(delim)
+    {
+        strncpy(ip, listen, delim - listen);
+        port = atoi(++delim);
+    }
+    else
+        return -1;
+
+    if(inet_pton(AF_INET, ip, &(addr->sin_addr)) <= 0)
+        return -1;
+
+    addr->sin_family = AF_INET;
+    addr->sin_port = htons(port);
+
+    return 0;
 }
--- src/config.h
+++ src/config.h
@@ -1,13 +1,39 @@
 #ifndef __CONFIG_H__
 #define __CONFIG_H__
 
+#include <string>
+using namespace  std;
+
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <libconfig/libconfig.h>
 #include "singleton.h"
 
 class MConfig : public Singleton<MConfig>
 {
     friend class Singleton<MConfig>;
 public:
+    struct Base
+    {
+        struct sockaddr_in worker_addr; // worker监听的端口
+        struct sockaddr_in master_addr; // master监听的端口
+        int workers;                    // worker进程的个数
+        int maxconn;                    // 最大允许的并行数
+        int shared_memory;              // 共享内存的大小
+    } base;
+
+    struct Log
+    {
+        string path;                    // 日志所在的目录
+        int32_t level;                  // debug日志级别(pain/debug/info)
+    } log;
+
     MConfig();
+
+    void loadConfFile(const char *file);
+
+private:
+    int getSockAddr(config_t* setting, const char *conf, struct sockaddr_in *addr);
 };
 
 #endif // __CONFIG_H__
