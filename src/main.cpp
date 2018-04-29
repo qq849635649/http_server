1fb44c9e85d494eadf662aa4b71aaeda138025c5
zhaoyf <zhaoyf@geewise.com>
2018-04-29 20:00:54 +0800

    添加加载配置的逻辑

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,40 @@
+
+#include <getopt.h>
+#include "config.h"
+
 int main(int argc, char * argv[])
 {
+    char ch;
+    //配置文件的位置
+    bool daemon = false;
+    const char *configPath = "./etc/base.conf";
+    const char *pidPath = "./proc/ucs.pid";
+    while(-1 != (ch = getopt(argc, argv,
+            "f:"    //手动指定配置文件的位置
+            "d"     //是否采取守护进程方式运行程序
+            "p:"	//进程文件
+            )))
+    {
+        switch(ch)
+        {
+        case 'f':
+            configPath = optarg;
+            break;
+        case 'd':
+            daemon = true;
+            break;
+        case 'p':
+            pidPath = optarg;
+            break;
+        default:
+            break;
+        }
+    }
+
+    if(daemon) {}
+
+    // 解析配置文件
+    MConfig::I().loadConfFile(configPath);
+
 	return 0;
 }
