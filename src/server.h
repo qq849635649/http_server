100622ef149b6c074b08f817cc5ec0258b5bc149
zhaoyf <zhaoyf@geewise.com>
2018-04-30 19:59:41 +0800

    新建两个http句柄，worker句柄和master句柄

--- src/server.h
+++ src/server.h
@@ -1,10 +1,25 @@
 #ifndef SERVER_H
 #define SERVER_H
 
-class Server
+#include <boost/noncopyable.hpp>
+#include "mainloop.h"
+
+class Server : boost::noncopyable
 {
+    // http句柄信息
+    typedef struct _HttpContext
+    {
+        int fd;                     // 套接字
+        struct evhttp *http_ctx;    // 根据套接字建立的http句柄
+    } HttpContext;
+
 public:
     Server();
+
+private:
+    HttpContext master_ctx_;        // Master进程套接字
+    HttpContext worker_ctx_;        // Worker进程套接字
+    struct event_base * evbase_;    // 异步事件控制
 };
 
 #endif // SERVER_H
